6d3b4a0da43b40c7cdcd405bc36cd7c5fa49a938
VedantaHazra <119790882+VedantaHazra@users.noreply.github.com>
2024-05-26 21:48:33 +0530

    Arrow updated

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -7,17 +7,22 @@ public class ArrowScript : MonoBehaviour
 {
     public bool shot = false; 
     [SerializeField] private float speed = 1f; 
+    [SerializeField] private float timeSpan = 30f; 
     private Vector3 vector;
+    private float time = 0f;
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        if(shot)
-        {
-            Vector3 position = transform.position + vector*speed;
-            transform.position = position;
+        if(!shot) { return; }
+        Vector3 position = transform.position + vector*speed;
+        transform.position = position;
+        time += Time.deltaTime;
+        if(time > timeSpan) {
+            Destroy(gameObject);
         }
+
     }
     public void Shot(Vector3 vect)
     {
